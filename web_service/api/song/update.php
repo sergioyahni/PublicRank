a5c8ef13b5dab7f5d782e6eb6e3f1c8b9ea03c02
Sergio Yahni <sergioyahni@gmail.com>
2020-02-20 21:44:19 +0200

    Update update.php

--- web_service/api/song/update.php
+++ web_service/api/song/update.php
@@ -6,6 +6,7 @@ header('Content-Type: application/json; charset=UTF-8');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, content-Type, Allow-Methods, Authorization, x-Requested With');
 
+if (!isset($_SESSION)) { die(json_encode(array('message' => 'ID Error',); }
 
 include_once '../../config/Database.php';
 include_once '../../models/Song.php';
